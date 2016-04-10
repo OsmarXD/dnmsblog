9210475d44868363570692092871f919d0f7a971
Osmar Dominguez <osmardcx@gmail.com>
2016-04-10 00:56:20 -0500

    use pluralize, add name and description to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160401105340) do
+ActiveRecord::Schema.define(version: 20160410052542) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -60,6 +60,8 @@ ActiveRecord::Schema.define(version: 20160401105340) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.text     "description"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
